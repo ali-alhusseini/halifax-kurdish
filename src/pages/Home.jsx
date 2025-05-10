9a885f3f65c708def44e1c4fcf04bbde8cce4ee5
Ali Alhusseini <hello@alialhusseini.com>
2025-05-10 20:04:40 -0300

    Add intro text and logo to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,14 @@
+import { useTranslation } from 'react-i18next'
+import logo from '../assets/logo.png'
+
 export default function Home() {
+  const { t } = useTranslation()
+
   return (
-    <div className="p-4">
-      {/* Empty home page for now */}
+    <div className="max-w-4xl mx-auto p-6 text-center">
+      <img src={logo} alt="Society Logo" className="mx-auto w-50 mb-6" />
+      <p className="mb-3">{t('intro.paragraph1')}</p>
+      <p className="mb-3">{t('intro.paragraph2')}</p>
     </div>
   )
 }
