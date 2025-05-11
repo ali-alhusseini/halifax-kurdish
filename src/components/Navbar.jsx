63ade6437805172ea934833a36bc528eb6b7536f
Ali Alhusseini <hello@alialhusseini.com>
2025-05-10 23:04:40 -0300

    Change language selector

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,19 +3,20 @@ import { useTranslation } from 'react-i18next'
 export default function Navbar() {
   const { i18n, t } = useTranslation()
 
-  const changeLanguage = (lng) => {
-    i18n.changeLanguage(lng)
+  const changeLanguage = () => {
+    if (i18n.language === 'en') {
+      i18n.changeLanguage('ku')
+    } else if (i18n.language === 'ku') {
+      i18n.changeLanguage('en')
+    }
   }
 
   return (
     <nav className="border-b shadow p-4 flex justify-between items-center">
       <h1 className="text-xl font-bold">{t('siteTitle')}</h1>
       <div className="space-x-2">
-        <button onClick={() => changeLanguage('en')} className="hover:underline font-semibold cursor-pointer">
-          {t('language.en')}
-        </button>
-        <button onClick={() => changeLanguage('ku')} className="hover:underline font-semibold cursor-pointer">
-          {t('language.ku')}
+        <button onClick={() => changeLanguage()} className="hover:underline font-semibold cursor-pointer">
+          {i18n.language === 'en' ? t('Kurdî') : t('English')}
         </button>
       </div>
     </nav>
