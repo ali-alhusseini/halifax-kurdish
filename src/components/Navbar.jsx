e18aaac11d52a29c96bc5964520d27a764076880
Ali Alhusseini <hello@alialhusseini.com>
2025-05-11 00:35:11 -0300

    Add hover effect to language button

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,11 +12,12 @@ export default function Navbar() {
   }
 
   return (
-    <nav className="border-b shadow p-4 flex justify-between items-center">
+    <nav className="border-b shadow p-6 flex justify-between items-center">
       <h1 className="text-xl font-bold">{t('siteTitle')}</h1>
       <div className="space-x-2">
-        <button onClick={() => changeLanguage()} className="hover:underline font-semibold cursor-pointer">
+        <button onClick={() => changeLanguage()} className="relative group font-semibold cursor-pointer">
           {i18n.language === 'en' ? t('Kurdî') : t('English')}
+          <span className="absolute left-0 -bottom-1 h-1 w-0 bg-[#00350E] transition-all duration-300 group-hover:w-full"></span>
         </button>
       </div>
     </nav>
