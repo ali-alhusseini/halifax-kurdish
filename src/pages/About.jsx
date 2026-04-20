977ae2c0645116e370177d3232088027565dca1a
Ali Alhusseini <hello@alialhusseini.com>
2026-04-20 17:29:13 -0300

    Add community image to about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,4 +1,5 @@
 import { useTranslation } from "react-i18next";
+import flagRaisingImage from "../assets/flag-raising.jpg";
 
 export default function About() {
     const { t } = useTranslation();
@@ -7,6 +8,14 @@ export default function About() {
         <section className="p-6 mx-auto">
             <h1 className="text-3xl md:text-5xl font-bold mb-4">{t("about.title")}</h1>
             <p className="mb-3 text-2xl md:text-3xl">{t("about.longDescription")}</p>
+            <div className="mt-6 flex flex-col">
+                <img
+                    src={flagRaisingImage}
+                    alt="Flag Raising"
+                    className="w-full md:w-5/6 h-auto rounded-lg shadow-md transition-transform duration-300 hover:scale-101"
+                />
+                <p className="text-base mt-4">{t("about.imageCaption")}</p>
+            </div>
         </section>
     )
 }
