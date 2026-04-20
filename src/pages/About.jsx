277afadd23b55dd2adad6bbe1946276468675085
Ali Alhusseini <hello@alialhusseini.com>
2026-04-20 17:43:51 -0300

    Update layout of About page for improved responsiveness

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,13 +8,13 @@ export default function About() {
         <section className="p-6 mx-auto">
             <h1 className="text-3xl md:text-5xl font-bold mb-4">{t("about.title")}</h1>
             <p className="mb-3 text-2xl md:text-3xl">{t("about.longDescription")}</p>
-            <div className="mt-6 flex flex-col">
+            <div className="mt-6 flex md:flex-row flex-col">
                 <img
                     src={flagRaisingImage}
                     alt="Flag Raising"
                     className="w-full md:w-5/6 h-auto rounded-lg shadow-md transition-transform duration-300 hover:scale-101"
                 />
-                <p className="text-base mt-4">{t("about.imageCaption")}</p>
+                <p className="text-base my-4 md:ml-4 md:self-end">{t("about.imageCaption")}</p>
             </div>
         </section>
     )
