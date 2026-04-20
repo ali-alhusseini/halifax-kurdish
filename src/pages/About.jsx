1cf34aa58a8d5e1a0e89e8791ab633ec2d75dde0
Ali Alhusseini <hello@alialhusseini.com>
2026-04-20 16:26:36 -0300

    Update About page layout and content

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,6 +6,7 @@ export default function About() {
     return (
         <section className="p-6 mx-auto">
             <h1 className="text-3xl md:text-5xl font-bold mb-4">{t("about.title")}</h1>
+            <p className="mb-3 text-2xl md:text-3xl">{t("about.longDescription")}</p>
         </section>
     )
 }
