57e86f689a5b952efdbde1c0fee4938d05eed7c1
Ali Alhusseini <hello@alialhusseini.com>
2026-04-20 17:38:10 -0300

    Enhance icon hover effect in NavigationCards component

--- src/components/NavigationCards.jsx
+++ src/components/NavigationCards.jsx
@@ -29,7 +29,7 @@ export default function NavigationCards() {
                 width="40"
                 height="40"
                 fill="currentColor"
-                className="bi bi-plus-circle"
+                className="bi bi-plus-circle hover:text-red-900 transition-colors duration-300"
                 viewBox="0 0 16 16"
               >
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
